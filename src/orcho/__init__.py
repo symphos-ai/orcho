268c040b233e2a87db1dd88439170e6e1a28971b
Eugen Kuzmin <jekccs@gmail.com>
2026-07-23 14:24:11 +0200

    fix(package): use installed version metadata directly
    Signed-off-by: Eugen Kuzmin <jekccs@gmail.com>

--- src/orcho/__init__.py
+++ src/orcho/__init__.py
@@ -1,9 +1,6 @@
 """Convenience installer package for the public Orcho command set."""
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
 __all__ = ["__version__"]
 
-try:
-    __version__ = version("orcho")
-except PackageNotFoundError:
-    __version__ = "0+unknown"
+__version__ = version("orcho")
